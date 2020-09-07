5fcaf38e6f5138c698685ff05dd17aa5d809610a
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-07 14:52:24 +0200

    Created static folder & added favicon.ico

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
-
+app.use(express.static("public"));
 
 //API Router
 app.use("/api", require('./routes/api/index'));
